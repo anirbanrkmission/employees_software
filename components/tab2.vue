9b6d335406f261bda1afc80e37cc4d5d06025713
Anirban Bal <36005368+anirbanrkmission@users.noreply.github.com>
2020-02-14 13:11:08 +0530

    Add files via upload

---
+++ components/tab2.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-row justify="center">
+    <v-expansion-panels
+    popout>
+      <v-expansion-panel
+        v-for="(e) in this.employees"
+        :key="e.id"
+      >
+        <v-expansion-panel-header>{{e.name }} {{ e.work}}</v-expansion-panel-header>
+        <v-expansion-panel-content>
+          
+          {{ (e.free)? "Completed": "In Progress" }}<br/>
+          Time Spent: {{ (((Date.now()-e.time)/(1000*60*60)) - 1).toFixed(0) }} Hrs.
+          {{(((Date.now() - e.time)/(1000*60))-((Date.now()-e.time)/(1000*60*60))).toFixed(2)}} Mins.
+
+        </v-expansion-panel-content>
+      </v-expansion-panel>
+    </v-expansion-panels>
+  </v-row>
+</template>
---
+++ components/work_list.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-app>
+    <v-content>
+      <p>Hello</p>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+    name: 'work_list',
+    data() {
+        
+    }
+}
+</script>
